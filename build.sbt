0308647ae66e92c10f08dc6557ceac0c0e044c49
yokra9 <53964890+yokra9@users.noreply.github.com>
2022-09-06 12:05:29 +0000

    テストコードを追加

--- build.sbt
+++ build.sbt
@@ -1,10 +1,10 @@
-val AkkaVersion = "2.6.8"
-val AkkaHttpVersion = "10.2.4"
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
-  "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
-  "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
-  "ch.qos.logback" % "logback-classic" % "1.2.3"
+  "com.typesafe.akka" %% "akka-actor-typed" % "2.6.8",
+  "com.typesafe.akka" %% "akka-stream" % "2.6.8",
+  "com.typesafe.akka" %% "akka-http" % "10.2.4",
+  "ch.qos.logback" % "logback-classic" % "1.2.3",
+  "org.scalactic" %% "scalactic" % "3.2.13",
+  "org.scalatest" %% "scalatest" % "3.2.13" % Test
 )
 
 enablePlugins(JavaAppPackaging)
---
+++ src/test/scala/sample.scala
@@ -0,0 +1,107 @@
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import org.scalatest.funsuite.AsyncFunSuite
+
+class MainTests extends AsyncFunSuite {
+
+  var step = 0
+  test(s"${step}: サーバを起動（main）") {
+    Main.main(Array(""))
+
+    assert(true)
+  }
+
+  step += 1
+  test(s"${step}: ステータスコードが200であること") {
+    implicit val system = ActorSystem(Behaviors.empty, s"step${step}")
+    implicit val executionContext = system.executionContext
+
+    val responseFuture: Future[HttpResponse] =
+      Http().singleRequest(HttpRequest(uri = "http://localhost:8080/index?q=1"))
+
+    responseFuture.map(response => {
+      println(response.status)
+      assert(response.status.intValue == 200)
+    })
+  }
+
+  step += 1
+  test(s"${step}: レスポンス本文が正しいこと（クエリあり）") {
+    implicit val system = ActorSystem(Behaviors.empty, s"step${step}")
+    implicit val executionContext = system.executionContext
+
+    val responseFuture: Future[HttpResponse] =
+      Http().singleRequest(HttpRequest(uri = "http://localhost:8080/index?q=1"))
+
+    val response = Await.result(responseFuture, 1000 millis)
+    Unmarshal(response)
+      .to[String]
+      .map(str => {
+        println(str)
+        assert(
+          str.equals("param: Map(q -> 1), user-agent: akka-http}")
+        )
+      })
+  }
+
+  step += 1
+  test(s"${step}: レスポンス本文が正しいこと（クエリなし）") {
+    implicit val system = ActorSystem(Behaviors.empty, s"step${step}")
+    implicit val executionContext = system.executionContext
+
+    val responseFuture: Future[HttpResponse] =
+      Http().singleRequest(HttpRequest(uri = "http://localhost:8080/index"))
+
+    val response = Await.result(responseFuture, 1000 millis)
+    Unmarshal(response)
+      .to[String]
+      .map(str => {
+        println(str)
+        assert(
+          str.equals("param: Map(), user-agent: akka-http}")
+        )
+      })
+  }
+
+  step += 1
+  test(s"${step}: 存在しないページのステータスコードが404であること") {
+    implicit val system = ActorSystem(Behaviors.empty, s"step${step}")
+    implicit val executionContext = system.executionContext
+
+    val responseFuture: Future[HttpResponse] =
+      Http().singleRequest(HttpRequest(uri = "http://localhost:8080/"))
+
+    responseFuture.map(response => {
+      println(response.status)
+      assert(response.status.intValue == 404)
+    })
+  }
+
+  step += 1
+  test(s"${step}: サーバを起動（Port指定）") {
+    sys.props("http.port") = "8081"
+    val serverbinding = Await.result(Main.start(), 1000 millis)
+
+    assert(serverbinding.localAddress.getPort() == 8081)
+  }
+
+  step += 1
+  test(s"${step}: 環境変数でポート指定ができること") {
+    implicit val system = ActorSystem(Behaviors.empty, s"step${step}")
+    implicit val executionContext = system.executionContext
+
+    val responseFuture: Future[HttpResponse] =
+      Http().singleRequest(HttpRequest(uri = "http://localhost:8081/index?q=1"))
+
+    responseFuture.map(response => {
+      println(response.status)
+      assert(response.status.intValue == 200)
+    })
+  }
+
+}
